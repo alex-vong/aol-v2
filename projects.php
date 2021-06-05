338a63dc1a37ba5de915307b080c419a41ed428a
Alex Vong <avong003@gmail.com>
2021-06-04 18:43:38 -0700

    projects php

---
+++ projects.php
@@ -0,0 +1,45 @@
+<?php
+
+ $responsive = [
+   "project" => "Responsive Layout",
+   "image" => "images/responsive.png",
+   "link" => "https://peprojects.dev/alpha-1/alexvong/responsive-layout/",
+   "tools" => "HTML, CSS, and PHP"
+ ];
+
+  $adoption = [
+   "project" => "Monster Adoption Agency",
+   "image" => "images/monster.png",
+   "link" => "https://peprojects.dev/alpha-1/alexvong/php-projects/monster/",
+   "tools" => "HTML, CSS, and PHP"
+ ];
+
+  $pokedex = [
+   "project" => "Pokedex",
+   "image" => "images/poke.png",
+   "link" => "https://peprojects.dev/alpha-1/alexvong/pokedex/",
+   "tools" => "HTML and CSS"
+ ];
+
+  $biz = [
+   "project" => "Business Card Website",
+   "image" => "images/biz-card.png",
+   "link" => "https://peprojects.dev/alpha-1/biz-card-website/",
+   "tools" => "HTML, CSS, PHP"
+ ];
+
+  $paw = [
+   "project" => "Responsive Layout",
+   "image" => "images/paw.png",
+   "link" => "https://peprojects.dev/alpha-1/alexvong/pawsitive-pets/",
+   "tools" => "HTML, CSS, PHP"
+ ];
+
+ $aol = [
+   "project" => "AOL (Alex On-Line)",
+   "image" => "images/AOL.png",
+   "link" => "file:///Users/alexvong/pe-websites/alex-on-line/index.html",
+   "tools" => "HTML and CSS"
+ ];
+
+$projectList = [$responsive, $adoption, $pokedex, $biz, $paw, $aol];
