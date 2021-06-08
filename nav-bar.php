0046d00a6d6f8d3ae9fcf005f3277b325a016d63
Alex Vong <avong003@gmail.com>
2021-06-07 19:18:16 -0700

    changed nav bar links

---
+++ nav-bar.php
@@ -0,0 +1,30 @@
+<nav>
+	<nav-bar>
+		<picture class="alex-man">
+			<a href="#">
+				<img src="images/alex-pow.svg" alt="">
+			</a>
+		</picture>
+		<menu-wrap>
+			<input type="checkbox" class="toggler" />
+			<div class="hamburger">
+				<div></div>
+			</div>
+			<menu>
+				<div>
+					<div>
+						<ul>
+							<li><a href="https://peprojects.dev/alpha-1/alexvong/aol-v2/#home">About</a></li>
+							<li><a href="https://peprojects.dev/alpha-1/alexvong/aol-v2/#writing">My Writing</a></li>
+							<li>
+								<a href="https://peprojects.dev/alpha-1/alexvong/aol-v2/#projects">Projects</a
+								>
+							</li>
+							<li><a href="https://peprojects.dev/alpha-1/alexvong/aol-v2/#contact">Contact</a></li>
+						</ul>
+					</div>
+				</div>
+			</menu>
+		</menu-wrap>
+	</nav-bar>
+</nav>
