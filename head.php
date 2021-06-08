66ade8a94eb8dd742f9ad914f417a25b8bf38035
Alex Vong <avong003@gmail.com>
2021-06-07 18:22:36 -0700

    added head php

---
+++ head.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="description" content="Alex Man Codes the Day!" />
+	<meta name="og:description" content="SUPER Responsive Layout Challenge" />
+	<meta property="og:image" content="https://peprojects.dev/alpha-1/alexvong/aol-v2/images/super-alex-meta.png" />
+	<meta property="og:title" content="Alex Man Codes the Day!" />
+	<link rel="shortcut icon" type="image/jpg" href="images/alex-pow.svg" />
+
+	<link rel="stylesheet" href="styles/styles.css" />
+	<link rel="stylesheet" href="styles/menu.css" />
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<title>Alex On-Line</title>
+</head>
