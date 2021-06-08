c398e86249128c6b3e5a472a2fbf6523b4b1fdaf
Alex Vong <avong003@gmail.com>
2021-06-07 18:22:29 -0700

    added footer

---
+++ footer.php
@@ -0,0 +1,41 @@
+<footer id="contact">
+<div class="inner-column">
+	<sections class="footer">
+		<div class="icons footer-box">
+			<p class="supporting-text">Contact Me:</p>
+			<ul>
+				<li><a href="" class="icons"><i class="fab fa-github-alt"></i> Github</a></li>
+				<li><a href="" class="icons"><i class="fab fa-twitter-square"></i> Twitter</a></li>
+				<li><a href="" class="icons"><i class="fab fa-instagram"></i> Instagram</a></li>
+				<li><a href="" class="icons"></a><i class="far fa-paper-plane"></i> Email</li>
+				<li><a href="" class="icons"></a><i class="fas fa-phone"></i> Phone</li>
+			</ul>
+		</div>
+
+		<div class="sign-in footer-box">
+			<p class="supporting-text">Sign In:</p>
+			<a href="">Sign In</a>
+		</div>
+
+		<div class="legal-stuff footer-box">
+			<p class="supporting-text">Legal Stuff:</p>
+			<ul>
+				<li><a href="">Legal Thing 1</a></li>
+				<li><a href="">Legal Thing 2</a></li>
+				<li><a href="">Legal Thing 3</a></li>
+				<li><a href="">Legal Thing 4</a></li>
+			</ul>
+		</div>
+
+		<div class="other-links footer-box">
+			<p class="supporting-text">Other Links:</p>
+			<ul>
+				<li><a href="">Some Link 1</a></li>
+				<li><a href="">Some Link 2</a></li>
+				<li><a href="">Some Link 3</a></li>
+				<li><a href="">Some Link 4</a></li>
+			</ul>
+		</div>
+	</sections>
+</div>
+</footer>
